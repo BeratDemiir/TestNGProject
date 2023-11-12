01f6b20efb3c7a21ea278bb5e6ab81fae0d251fe
Berat Demir <brtdmr2726@gmail.com>
2023-11-12 21:43:40 +0300

    12.11.23

--- src/test/java/techproed/utilities/ListenersRetry.java
+++ src/test/java/techproed/utilities/ListenersRetry.java
@@ -5,7 +5,7 @@ import org.testng.ITestResult;
 public class ListenersRetry implements IRetryAnalyzer {
     //    BU SINIF SADECE FAIL OLAN TEST CASELERE TEKRAR CALISTIRIR
     private int retryCount = 0;
-    //    maxRetryCount EK OLARAK CALISMA SAYISI. BU ORNEKDE FAIL OLNA TESTLER EXTRA 1 KEZ DAHA CALISIR
+    //    maxRetryCount EK OLARAK CALISMA SAYISI. BU ORNEKDE FAIL OLAN TESTLER EXTRA 1 KEZ DAHA CALISIR
     private static final int maxRetryCount = 1;
     @Override
     public boolean retry(ITestResult result) {
