e17e01ec1e6a91ff22b293cbab68b8dfadeae526
Berat Demir <brtdmr2726@gmail.com>
2024-02-03 12:38:04 +0300

    03.02.24

--- src/test/java/techproed/utilities/ListenersRetryAnalyzer.java
+++ src/test/java/techproed/utilities/ListenersRetryAnalyzer.java
@@ -7,11 +7,11 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
 public class ListenersRetryAnalyzer implements IAnnotationTransformer {
-        //    Bu sinifin amaci, TUM FAILE TEST CASELERI BIR KEZ DAHA AUTOMATIK OLARAK XML FILE ILE RUN ETMEK
+    //    Bu sinifin amaci, TUM FAILE TEST CASELERI BIR KEZ DAHA AUTOMATIK OLARAK XML FILE ILE RUN ETMEK
 //    BU SINIF ListenersRetry sinifini xml de kullanabilmek icin olusturuldu
-        @Override
-        public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
+    @Override
+    public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
 //        Bu sinif ListenersRetry util classina baglidir
-            annotation.setRetryAnalyzer(ListenersRetry.class);
-        }
- }
+        annotation.setRetryAnalyzer(ListenersRetry.class);
+    }
+}
